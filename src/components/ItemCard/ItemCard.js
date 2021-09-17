d6d3d2340153dce774a55c1c63fbfb0973c59901
AranBeitia <arantza.beitia@gmail.com>
2021-09-17 16:43:43 +0200

    update repo

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -16,7 +16,7 @@ function ItemCard({
   img,
   title,
   shortDescription,
-  isFavorite,
+  isFavourite,
   upVotes,
   downVotes,
   handleDownVote,
@@ -38,38 +38,37 @@ function ItemCard({
   }
 
   return (
-    <article className="ItemCard col col-12 col-md-6 col-lg-4">
-      <header>
-        <div className="ItemCard__image-wrapper">
-          <img src={img} className="ItemCard__image" alt={title} />
-          <FavoriteIconButton
-            handleSetFavorite={onSetFavorite}
-            isFavorite={isFavorite}
-          />
-        </div>
-        <h2 className="ItemCard__title">{title}</h2>
+    <article className="item-card col col-12 col-md-6 col-lg-4">
+      <header className="position-relative">
+        <img src={img} className="item-card__image" alt={title} />
+        <FavoriteIconButton
+          handleSetFavorite={onSetFavorite}
+          isFavourite={isFavourite}
+        />
+        <h2>{title}</h2>
       </header>
+
       <Divider />
-      <p className="ItemCard__description">{shortDescription}</p>
+      <p>{shortDescription}</p>
       <Divider />
-      <footer className="ItemCard__meta">
-        <div className="ItemCard__icons">
-          <div className="ItemCard__icon-row">
-            <IconButton aria-label="up vote product" handleClick={onUpVote}>
-              <ThumbUp />
-            </IconButton>
-            <p className="ItemCard__icon-txt">{upVotes.currentValue}</p>
-          </div>
-          <div className="ItemCard__icon-row">
+      <footer className="d-flex justify-content-between">
+        <div className="d-flex justify-content-start">
+          <div className="d-flex align-items-center">
             <IconButton aria-label="down vote product" handleClick={onDownVote}>
               <ThumbDown />
             </IconButton>
-            <p className="ItemCard__icon-txt">{downVotes.currentValue}</p>
+            <p className="mx-1">{downVotes.currentValue}</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <IconButton aria-label="up vote product" handleClick={onUpVote}>
+              <ThumbUp />
+            </IconButton>
+            <p className="mx-1">{upVotes.currentValue}</p>
           </div>
         </div>
-        <div className="ItemCard__icon-row">
-          <Button onClick={onAddToCart}>Add to cart</Button>
-        </div>
+        <Button name="add to cart" onClick={onAddToCart}>
+          Add to cart
+        </Button>
       </footer>
     </article>
   );
